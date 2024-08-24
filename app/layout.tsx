d58c7924d9d5b31862786ba3041cc85b3dd23dac
CHEGEBB <chegephil24@gmail.com>
2024-08-24 12:20:05 +0300

    chore: Update npm dependencies and tailwind configuration, add new packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import {cn} from "@/lib/utils";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 const fontoutfit = Outfit({ 
   subsets: ["latin"],
   weight: ['300' , '400' ,'500', '600' ,'700'],
   variable: '--font-outfit'
+
 
  });
 
@@ -21,7 +23,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-outfit antialiased', fontoutfit.variable)}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-outfit antialiased', fontoutfit.variable)}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          </body>
     </html>
   );
 } 
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl underline">
-        Home
-      </h1>
+    <div className="flex h-screen max-h-screen">
+    <section className="remove-scrollbar container my-auto">
+      <div className="sub-container max-w-[496px]">
+          <Image 
+          src="/assets/icons/png/logo3.png "
+          alt="HealthMaster"
+          quality={100}
+          width={1000}
+          height={1000}
+          className="mb-12 h-10 w-fit"
 
+          />
+      </div>
+
+    </section>
     </div>
   );
 }
