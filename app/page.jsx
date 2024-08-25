d36b1f7abcf0b2e2b1c912c48a6aedef51b5311e
CHEGEBB <chegephil24@gmail.com>
2024-08-25 10:59:46 +0300

    refactor: Update import statements for Button and Input components

--- app/page.jsx
+++ app/page.jsx
@@ -1,86 +1,91 @@
-"use client"
+"use client";
 import Image from "next/image";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
-import "../sass/auth.scss";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import "../sass/auth.scss";
 
 export default function Home() {
   const [focusedInput, setFocusedInput] = useState(null);
 
   return (
-    (<div className="container">
-      <div className="flex min-h-screen form-container">
+    <div className="container">
+      <div className="form-container">
         <div className="form-title">
           <h1>Sign Up</h1>
         </div>
         <form>
-        <div className={`form-group ${focusedInput == 'name' ? 'focused': ''}`}>
-        <label html for="name">Full name</label>
-        <div className="input-wrapper">
-          <FontAwesomeIcon icon={faUser} className="input-icon" />
-          <Input 
-            type="text" 
-            id="name" 
-            required 
-            onFocus={() => setFocusedInput('name')}
-            onBlur={() => setFocusedInput(null)}
-          />
-          
-        </div>
-        </div>
+          <div className={`form-group ${focusedInput === 'name' ? 'focused' : ''}`}>
+            <label htmlFor="name">Full Name</label>
+            <div className="input-wrapper">
+              <FontAwesomeIcon icon={faUser} className="input-icon" />
+              <Input
+                type="text"
+                id="name"
+                required
+                onFocus={() => setFocusedInput('name')}
+                onBlur={() => setFocusedInput(null)}
+              />
+            </div>
+          </div>
+
           <div className={`form-group ${focusedInput === 'email' ? 'focused' : ''}`}>
             <label htmlFor="email">Email</label>
             <div className="input-wrapper">
               <FontAwesomeIcon icon={faEnvelope} className="input-icon" />
-              <Input 
-                type="email" 
-                id="email" 
-                required 
+              <Input
+                type="email"
+                id="email"
+                required
                 onFocus={() => setFocusedInput('email')}
                 onBlur={() => setFocusedInput(null)}
               />
             </div>
           </div>
+
           <div className={`form-group ${focusedInput === 'password' ? 'focused' : ''}`}>
             <label htmlFor="password">Password</label>
             <div className="input-wrapper">
               <FontAwesomeIcon icon={faLock} className="input-icon" />
-              <Input 
-                type="password" 
-                id="password" 
-                required 
+              <Input
+                type="password"
+                id="password"
+                required
                 onFocus={() => setFocusedInput('password')}
                 onBlur={() => setFocusedInput(null)}
               />
             </div>
           </div>
+
           <div className={`form-group ${focusedInput === 'confirmPassword' ? 'focused' : ''}`}>
             <label htmlFor="confirmPassword">Confirm Password</label>
             <div className="input-wrapper">
               <FontAwesomeIcon icon={faLock} className="input-icon" />
               <Input
-                type="password" 
-                id="confirmPassword" 
-                required 
+                type="password"
+                id="confirmPassword"
+                required
                 onFocus={() => setFocusedInput('confirmPassword')}
                 onBlur={() => setFocusedInput(null)}
               />
             </div>
           </div>
+
           <div className="form-group">
-          <Button type="submit" className="btn btn-primary">
-          sign up
+            <Button type="submit" className="btn btn-primary">
+              Sign Up
             </Button>
           </div>
         </form>
+
         <div className="form-group">
           <p>Already have an account? <a href="/login">Login</a></p>
         </div>
       </div>
-      <div className="min-h-screen container-image">
+
+      <div className="container-image">
         <Image
           src="/assets/images/background-1.webp"
           width={1000}
@@ -88,6 +93,6 @@ export default function Home() {
           alt="Background"
         />
       </div>
-    </div>)
+    </div>
   );
 }
