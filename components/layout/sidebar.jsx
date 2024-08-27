e91ea2a962a5962088e30a3af790190ea2350208
CHEGEBB <chegephil24@gmail.com>
2024-08-27 20:45:27 +0300

    feat: Add @mui/icons-material dependency and update sidebar component

--- components/layout/sidebar.jsx
+++ components/layout/sidebar.jsx
@@ -1,15 +1,20 @@
-import Link from 'next/link'
-import React from 'react'
+import { LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 export default function sidebar() {
   return (
     <div className="sidebar-container">
-    <Link href="/dashboard">
-    </Link>
-    
-     
-    
-        
+      <div className="side-item">
+        <Link href="/dashboard">
+          <div className="side-con">
+            <LayoutDashboard className="sidebar-icon" size={32} />
+          </div>
+          <div className="side-info">
+            <h1>Dashboard</h1>
+          </div>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
