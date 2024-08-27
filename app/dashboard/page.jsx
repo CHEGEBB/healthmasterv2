25769c36ba62921ebf92420bc3632b694f63b78b
CHEGEBB <chegephil24@gmail.com>
2024-08-27 20:35:22 +0300

    refactor: Update select dropdown styles and options in PersonalInfo form

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import "../../sass/dashboard.scss"
+import Sidenav from '../../components/layout/sidebar';
+
 
 export default function page() {
   return (
-    <div>page</div>
+<div className="container bg-[#083A3C] !important">
+<div className="w-20 sidenav">
+  <Sidenav/>
+</div>
+<div className="min-h-screen container-fluid">
+<h>
+  content will go here
+</h>
+  
+</div>
+      
+    </div>
   )
 }
---
+++ components/layout/sidebar.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function sidebar() {
+  return (
+    <div className="sidebar-container">
+    <Link href="/dashboard">
+    </Link>
+    
+     
+    
+        
+    </div>
+  )
+}
