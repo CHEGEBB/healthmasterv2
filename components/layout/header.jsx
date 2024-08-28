d36489db557c01798b7296fef90646d1a2f7bace
CHEGEBB <chegephil24@gmail.com>
2024-08-28 16:25:40 +0300

    refactor: Update header component and styles

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -1,41 +1,26 @@
 import React from 'react';
-import { Search, Bell, User, Calendar, Heart, HelpCircle } from 'lucide-react';
+import { Search, Bell, User } from 'lucide-react';
 import "../../sass/header.scss";
 
-const HealthMasterHeader = () => {
+const Header = () => {
   return (
-    <header className="healthMaster-header">
-      <div className="healthMaster-header__left">
-        <div className="healthMaster-header__logo">
-          <Heart className="healthMaster-header__logo-icon" />
-          <h1 className="healthMaster-header__title">HealthMaster</h1>
-        </div>
-        <div className="healthMaster-header__search">
-          <input type="text" placeholder="Search patients, records..." className="healthMaster-header__search-input" />
-          <Search className="healthMaster-header__search-icon" />
-        </div>
+    <header className="header">
+      <div className="header__left">
+        <h1 className="header__title">Overview Conditions</h1>
       </div>
-      <div className="healthMaster-header__center">
-        <button className="healthMaster-header__tab">Dashboard</button>
-        <button className="healthMaster-header__tab healthMaster-header__tab--active">Patients</button>
-        <button className="healthMaster-header__tab">Appointments</button>
-        <button className="healthMaster-header__tab">Reports</button>
-      </div>
-      <div className="healthMaster-header__right">
-        <button className="healthMaster-header__icon-btn">
-          <Calendar />
-        </button>
-        <button className="healthMaster-header__icon-btn">
-          <HelpCircle />
-        </button>
-        <button className="healthMaster-header__icon-btn">
+      <div className="header__right">
+        <div className="header__search">
+          <input type="text" placeholder="Search..." className="header__search-input" />
+          <Search className="header__search-icon" />
+        </div>
+        <button className="header__icon-btn">
           <Bell />
         </button>
-        <div className="healthMaster-header__profile">
-          <button className="healthMaster-header__icon-btn healthMaster-header__user-btn">
+        <div className="header__profile">
+          <button className="header__icon-btn header__user-btn">
             <User />
           </button>
-          <div className="healthMaster-header__profile-dropdown">
+          <div className="header__profile-dropdown">
             <a href="#">Profile</a>
             <a href="#">Settings</a>
             <a href="#">Logout</a>
@@ -46,4 +31,4 @@ const HealthMasterHeader = () => {
   );
 };
 
-export default HealthMasterHeader;
+export default Header;
