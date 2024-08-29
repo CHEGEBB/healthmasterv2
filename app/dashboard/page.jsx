1a0aa7af292eff5c3b8619739a70c04b2458d258
CHEGEBB <chegephil24@gmail.com>
2024-08-29 17:50:53 +0300

    refactor: Update dashboard page layout to display health cards in a grid

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "../../sass/dashboard.scss";
 import Sidenav from '../../components/layout/sidebar';
 import Header from "../../components/layout/header";
+import Cards from '../../components/cards/Cards';
 export default function DashboardPage() {
   return (
     <div className="container bg-[#083A3C] !important">
@@ -13,7 +14,8 @@ export default function DashboardPage() {
           <Header />
         </div>
         <div className="min-h-screen container-fluid">
-          <h1 className="mb-4 text-2xl font-bold">Health Dashboard</h1>
+          <h1 className="mb-4 text-2xl font-bold">Health Diagnosis Overview</h1>
+          <Cards/>
           </div>
         </div>
       </div>
--- components/cards/Cards.jsx
+++ components/cards/Cards.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Cards() {
   return (
     <div className="cards-container">
+    
         
     </div>
   )
