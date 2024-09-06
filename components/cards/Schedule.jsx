6bb96df7ccae3e71378bc14eff53925215456580
CHEGEBB <chegephil24@gmail.com>
2024-09-06 12:28:45 +0300

    refactor: Add lazy loading for user profile image in Schedule component

--- components/cards/Schedule.jsx
+++ components/cards/Schedule.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../../sass/schedule.scss"
-import { ArrowRight, CalendarDays } from 'lucide-react'
+import { ArrowRight, CalendarDays, MessageCircle, MessageCircleMore, PhoneCall } from 'lucide-react'
+import Image from 'next/image'
 
 function Schedule() {
   return (
@@ -17,6 +18,32 @@ function Schedule() {
     <div className="arrow">
         <ArrowRight size={24} />
     </div>
+    </div>
+    <div className="schedule-data">
+        <div className="schedule-item">
+            <div className="doctor-info">
+                <div className="doc-image">
+                    <Image
+                    src="/assets/images/1.png"
+                    alt="Doctor"
+                    width={100}
+                    height={100}
+                    />
+                </div>
+                <div className="doc-profile">
+                    <h3>Dr Emily Johnson</h3>
+                    <p>Pediatrician</p>
+                </div>
+            </div>
+            <div className="access">
+                <div className="call">
+                    <PhoneCall size={24} />
+                </div>
+                <div className="message">
+                    <MessageCircleMore size={24} />
+                </div>
+            </div>
+        </div>
     </div>
 
     </div>
